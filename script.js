6fe8d3e79a66b0d8b58d5c8526494f39ce91ae61
John <bramleyjl@gmail.com>
2017-03-09 22:38:41 +0800

    2nd day work

--- script.js
+++ script.js
@@ -1,32 +1,104 @@
 $(document).ready(function(){
 
-	var table = $('<table></table>');
-	$('#box').append(table)
+var rowNum = 40;
+var box = 960;
 
-	var rowNum = 16;
+$("#box").css("height", box + "px");
+$("#box").css("width", box + "px")
+
+var table = $('<table></table>');
+	$('#box').append(table);
+
+var gridMaker = function() {
 	for (var x = 0; x < rowNum; x++) {
 
 		var row = $('<tr></tr>');
 		$(table).append(row);
 
-		var colNum = 16;
-		for (var i = 0; i < colNum; i++) {
-			var block = $("<div></div>");
+		for (var i = 0; i < rowNum; i++) {
+			var block = $('<div class="pixel"></div>');
 			$(row).append(block);
 		};
 	};
-	
+	};
 
-	$("tr > div").hover(
+var pixSize = function() {
+	$(".pixel").css("height", box / rowNum + "px");
+	$(".pixel").css("width", box / rowNum + "px");
+}
+
+gridMaker(rowNum);
+pixSize(rowNum)
+
+$(".pixel").hover(
+	function() {
+	$(this).addClass("mouseOver");
+});
+
+//Reset switch
+
+$("#clear").click(function() {
+		$("tr").remove();
+		gridMaker(rowNum);
+		pixSize(rowNum);
+
+		$(".pixel").hover(
 		function() {
+		$(this).addClass("mouseOver");
+		});
+	});
+
+//Change resolution and reset switch
+
+$("#resolution").click(function() {
+		rowNum = prompt("How many squares would you like? (Choose between 10-100)");
+			
+		if(isNaN(rowNum) === true) {
+			alert("Please choose a real number!")
+			return;
+		}
+
+		else if(rowNum < 10) {
+			alert("Please choose a larger number.")
+			return;
+		}
+
+		else if(rowNum > 100) {
+			alert("Please choose a smaller number.")
+			return;
+		}
+
+		else {
+
+		$("tr").remove();
+		gridMaker(rowNum);
+		pixSize(rowNum);
+
+		$(".pixel").hover(function() {
 			$(this).addClass("mouseOver");
 		});
+		};
+	});
 
-	$("button").click(
-		function() {
-			var reset = prompt("How many squares would you like?");
-			$("tr > div").removeClass("mouseOver");
-			rowNum = reset;
-			colNum = reset;
-		})
+//Color switch
+
+$("#colors").click(function(){
+		$(".pixel").css("background-color", "#ff3300");
+		$(".mouseOver").css("background-color", "#0000ff");
+		
+		$(".pixel").hover(function() {
+		$(this).css("background-color", "#0000ff");
+		});
+	});
+
+//Black/white switch
+
+$("#blackWhite").click(function(){
+		$(".pixel").css("background-color", "#000000");
+		$(".mouseOver").css("background-color", "#ffffff");
+
+		$(".pixel").hover(function() {
+		$(this).css("background-color", "#ffffff");
+		});
+	});
 });
