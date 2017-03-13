d5556b237794bf67ad104a69706a2447498f71f6
John <bramleyjl@gmail.com>
2017-03-13 17:10:13 +0800

    line break and random color update

--- script.js
+++ script.js
@@ -2,23 +2,12 @@ $(document).ready(function(){
 
 var rowNum = 40;
 var box = 960;
-<<<<<<< HEAD
 
 $("#box").css("height", box + "px");
-$("#box").css("width", box + "px")
+$("#box").css("width", box + "px");
 
 var table = $('<table></table>');
 	$('#box').append(table);
-=======
-
-$("#box").css("height", box + "px");
-$("#box").css("width", box + "px")
-
-var table = $('<table></table>');
-	$('#box').append(table)
-
-
->>>>>>> 4f6194f35694082447f8bfaa40d527e82d844bbc
 
 var gridMaker = function() {
 	for (var x = 0; x < rowNum; x++) {
@@ -26,26 +15,26 @@ var gridMaker = function() {
 		var row = $('<tr></tr>');
 		$(table).append(row);
 
-		for (var i = 0; i < rowNum; i++) {
+		for (var j = 0; j < rowNum; j++) {
 			var block = $('<div class="pixel"></div>');
 			$(row).append(block);
-		};
+		};	
+		
 	};
 	};
 
-<<<<<<< HEAD
 var pixSize = function() {
 	$(".pixel").css("height", box / rowNum + "px");
 	$(".pixel").css("width", box / rowNum + "px");
-}
+	}
 
 gridMaker(rowNum);
-pixSize(rowNum)
+pixSize(rowNum);
 
 $(".pixel").hover(
 	function() {
 	$(this).addClass("mouseOver");
-});
+	});
 
 //Reset switch
 
@@ -54,41 +43,28 @@ $("#clear").click(function() {
 		gridMaker(rowNum);
 		pixSize(rowNum);
 
-		$(".pixel").hover(
-=======
-
-
-var pixSize = function() {
-	$(".pixel").css("height", box / rowNum + "px");
-	$(".pixel").css("width", box / rowNum + "px");
-}
-
-gridMaker(rowNum);
-pixSize(rowNum)
-
-$(".pixel").hover(
->>>>>>> 4f6194f35694082447f8bfaa40d527e82d844bbc
-		function() {
+		$(".pixel").hover(function() {
 		$(this).addClass("mouseOver");
 		});
 	});
 
+
 //Change resolution and reset switch
 
 $("#resolution").click(function() {
-		rowNum = prompt("How many squares would you like? (Choose between 10-100)");
+		rowNum = prompt("How many squares would you like? (Choose between 9-99)");
 			
 		if(isNaN(rowNum) === true) {
 			alert("Please choose a real number!")
 			return;
 		}
 
-		else if(rowNum < 10) {
+		else if(rowNum < 9) {
 			alert("Please choose a larger number.")
 			return;
 		}
 
-		else if(rowNum > 100) {
+		else if(rowNum > 99) {
 			alert("Please choose a smaller number.")
 			return;
 		}
@@ -105,81 +81,34 @@ $("#resolution").click(function() {
 		};
 	});
 
-<<<<<<< HEAD
 //Color switch
 
+function RandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+	};
+
 $("#colors").click(function(){
-		$(".pixel").css("background-color", "#ff3300");
-		$(".mouseOver").css("background-color", "#0000ff");
-		
+		$(".mouseOver").each(function() {
+			$(this).css("background-color", RandomColor());
+			});
 		$(".pixel").hover(function() {
-		$(this).css("background-color", "#0000ff");
+		$(this).css("background-color", RandomColor() );
 		});
 	});
 
 //Black/white switch
 
 $("#blackWhite").click(function(){
-		$(".pixel").css("background-color", "#000000");
 		$(".mouseOver").css("background-color", "#ffffff");
 
 		$(".pixel").hover(function() {
 		$(this).css("background-color", "#ffffff");
 		});
 	});
-=======
-$("#clear").click(
-	function() {
-		$(".pixel").removeClass("mouseOver");
-	});
 
-$("#resolution").click(
-	function() {
-		rowNum = prompt("How many squares would you like? (Choose between 10-100)");
-			
-		if(isNaN(rowNum) === true) {
-			alert("Please choose a real number!")
-			return;
-		}
-
-		else if(rowNum < 10) {
-			alert("Please choose a larger number.")
-			return;
-		}
-
-		else if(rowNum > 100) {
-			alert("Please choose a smaller number.")
-			return;
-		}
-
-		else {
-		$("tr").remove();
-
-		gridMaker(rowNum);
-		pixSize(rowNum);
-
-		$(".pixel").hover(
-		function() {
-		$(this).addClass("mouseOver");
-		});
-		}
-	})
-
-//Write colors functionality here
-
-$("#colors").click(
-	function(){
-		$(".pixel").css("background-color", "#ff3300");
-		$(".mouseOver").css("background-color", "#0000ff");
-	});
-
-//Write black/white functionality here
-
-$("#blackWhite").click(
-	function(){
-		$(".pixel").css("background-color", "#000000");
-		$(".mouseOver").css("background-color", "#ffffff");
-	});
-
->>>>>>> 4f6194f35694082447f8bfaa40d527e82d844bbc
 });
